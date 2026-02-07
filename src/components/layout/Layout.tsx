4670a381470c5a8d96bc252e350b32e5ef225d7a
raghav-kapoor27 <raghavkapoor27rk@gmail.com>
2026-02-07 22:05:34 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,12 +5,13 @@ import { Footer } from "./Footer";
 interface LayoutProps {
   children: ReactNode;
   hideFooter?: boolean;
+  hideHeader?: boolean;
 }
 
-export function Layout({ children, hideFooter = false }: LayoutProps) {
+export function Layout({ children, hideFooter = false, hideHeader = false }: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
+      {!hideHeader && <Header />}
       <main className="flex-1">{children}</main>
       {!hideFooter && <Footer />}
     </div>
